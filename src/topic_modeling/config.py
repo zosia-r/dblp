e2de2780040f41324c9bb9a79b87ce69769519fd
Zofia Różańska <280526@student.pwr.edu.pl>
2026-04-26 14:38:53 +0200

    refactor: change location oftopic model files

--- src/topic_modeling/config.py
+++ src/topic_modeling/config.py
@@ -15,7 +15,7 @@ CUSTOM_STOP_WORDS = list(ENGLISH_STOP_WORDS) + [
 
 DB_PATH         = Path("data/processed/dblp.db")
 DB_PATH.parent.mkdir(parents=True, exist_ok=True)
-MODEL_PATH      = Path("results/bertopic_model")
+MODEL_PATH      = Path("data/models/bertopic_model")
 MODEL_PATH.parent.mkdir(parents=True, exist_ok=True)
-EMBEDDINGS_PATH = Path("results/embeddings.npy")
+EMBEDDINGS_PATH = Path("data/models/embeddings.npy")
 EMBEDDINGS_PATH.parent.mkdir(parents=True, exist_ok=True)
